cd9126f256389d9e61b418759c31a67dd5bd52c5
arthurbochicruz <173960792+arthurbochicruz@users.noreply.github.com>
2025-07-05 00:31:03 -0300

    Update server.js

--- Node_API/server.js
+++ Node_API/server.js
@@ -2,8 +2,18 @@ import express from 'express'
 
 const app = express()
 
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    console.log(req)
+
+    res.send('Ok request')
+})
+
 app.get('/usuarios', (req, res) => {
+
     res.send('Tá dando bom aqui')
 })
+
 
 app.listen(3001)
