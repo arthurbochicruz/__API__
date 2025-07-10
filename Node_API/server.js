3ff32794d0df3285a9287613cfd82a3942c18c39
arthurbochicruz <173960792+arthurbochicruz@users.noreply.github.com>
2025-07-09 23:14:18 -0300

    Commit

--- Node_API/server.js
+++ Node_API/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import prisma from './generated/prisma/index.js';
-import { PrismaClient } from './generated/prisma'
+//import prisma from './generated/prisma/index.js';
+import { PrismaClient } from './generated/prisma/index.js'
  
 
 const prisma = new PrismaClient()
@@ -10,7 +10,6 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
 
 app.post('/usuarios', async (req, res) => {
 
@@ -25,11 +24,39 @@ app.post('/usuarios', async (req, res) => {
     res.status(201).json(req.body) 
 
 })
+app.put('/usuarios/:id', async (req, res) => {
+        
+       await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+        })
 
-app.get('/usuarios', (req, res) => {
+    res.status(201).json(req.body) 
+
+})
+
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
     res.json(users)
     res.status(200).json(users)
 })
 
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
 
-app.listen(3001)
+    })
+    res.status(200).json({message: 'Usuario Deletado Com Sucesso'}) 
+})
+
+
+app.listen(5001)
